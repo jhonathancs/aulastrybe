b8e798d9784fee2be97c7abc00de82531e9a8ae6
jhonathancs <jhonathan_jcs@hotmail.com>
2022-03-12 10:28:09 -0400

    22-4

--- hello-express/index.js
+++ hello-express/index.js
@@ -1,13 +1,52 @@
-const express = require("express");
-
+const express = require('express');
 const app = express();
 
-app.get('/hello', handleHelloWorldRequest);
+const recipes = [
+  { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
+  { id: 2, name: 'Macarrão a Bolonhesa', price: 35.0, waitTime: 25 },
+  { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
+];
 
-app.listen(3002, () => {
-    console.log('aplicacao ouvindo na porta 3001');
-})
+// const drinks = [
+//     { id: 1, name: 'Refrigerante Lata', price: 5.0 },
+//     { id: 2, name: 'Refrigerante 600ml', price: 8.0 },
+//     { id: 3, name: 'Suco 300ml', price: 4.0 },
+//     { id: 4, name: 'Suco 1l', price: 10.0 },
+//     { id: 5, name: 'Cerveja Lata', price: 4.5 },
+//     { id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
+// ];
 
-function handleHelloWorldRequest(req, res) {
-    res.status(200).send('hello World!');
-}
+// app.get('/drinks', function (req, res) {
+//     res.json(drinks);
+//   });
+
+app.get('/recipes', function (req, res) {
+  res.json(recipes);
+});
+
+app.get('/recipes/:id', function (req, res) {
+    const { id } = req.params;
+    const recipe = recipes.find((r) => r.id === parseInt(id));
+  
+    if (!recipe) return res.status(404).json({ message: 'Recipe not found!'});
+  
+    res.status(200).json(recipe);
+  });
+
+app.listen(3004, () => {
+  console.log('Aplicação ouvindo na porta 3001');
+});
+
+// const express = require("express");
+
+// const app = express();
+
+// app.get('/helloWorld', handleHelloWorldRequest);
+
+// app.listen(3002, () => {
+//     console.log('aplicacao ouvindo na porta 3001');
+// })
+
+// function handleHelloWorldRequest(req, res) {
+//     res.status(200).send('Bem vindo ao express');
+// }
